a09cc828fb3d164c77627422a0167dd41296edb0
Flore Hervy <florehervy@yahoo.fr>
2022-10-13 16:59:37 +0200

    essais ajout section create post

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 // Créer un schéma de données avec les informations nécessaires pour chaque post
 const postSchema = mongoose.Schema({
   userId: { type: String, required: true },
-  name: { type: String, required: true },
+  /*name: { type: String, required: true },*/
   /*manufacturer: { type: String, required: true },*/
   message: { type: String, required: true },
   /*mainPepper: { type: String, required: true },*/
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,8 @@
 import { useNavigate, Link } from "react-router-dom";
 import { useContext } from "react";
 import AuthContext from "../context/AuthProvider";
+import { faRightFromBracket, faImage, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Home = () => {
     const { setAuth } = useContext(AuthContext);
@@ -14,22 +16,60 @@ const Home = () => {
     }
 
     return (
-        <section>
-            <h1>Accueil</h1>
-            <br />
-            <p>Vous êtes connecté!</p>
-            <br />
-            <Link to="/editor">Aller à la page d'éditeur</Link>
-            <br />
-            <Link to="/admin">Aller à la page d'administrateur</Link>
-            <br />
-            <Link to="/lounge">Aller au Lounge</Link>
-            <br />
-            <Link to="/linkpage">Aller à la page de lien</Link>
-            <div className="flexGrow">
-                <button onClick={logout}>Se déconnecter</button>
-            </div>
-        </section>
+        <main>
+            <section>
+                <h1>Accueil</h1>
+                <br />
+                <p>Vous êtes connecté !</p>
+                <br />
+                <Link to="/editor">Aller à la page d'éditeur</Link>
+                <br />
+                <Link to="/admin">Aller à la page d'administrateur</Link>
+                <br />
+                <Link to="/lounge">Aller au Lounge</Link>
+                <br />
+                <Link to="/linkpage">Aller à la page de lien</Link>
+                <div className="flexGrow">
+                    <button onClick={logout}>
+                        Se déconnecter
+                        <FontAwesomeIcon icon={faRightFromBracket} />
+                    </button>
+                </div>
+            </section>
+
+            <section id="create-post">
+                <form>
+					<div class="create-post__div">
+						<textarea
+							class="create-post__posttextarea"
+							v-model="myText"
+							placeholder="Que voulez-vous partager ?"
+							aria-label="Un champ de texte pour créer un post"
+						></textarea>
+					</div>
+					
+					<div class="create-post__div create-post__fileinput">
+						<label for="myImage"
+							>Télécharger un fichier
+							<FontAwesomeIcon icon={faImage} />
+							<input
+								type="file"
+								id="myImage"
+								change="onFileChange"
+							/>
+						</label>
+						<button
+							class="create-post__btn"
+							type="submit"
+						>
+							Publier
+                            <FontAwesomeIcon icon={faPaperPlane} />
+						</button>
+					</div>
+				</form>
+            </section>
+        </main>
+        
     )
 }
 
