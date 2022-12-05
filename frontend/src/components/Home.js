af9b1d91022731cd79f7c0dcc53edd0991fb643d
Flore Hervy <florehervy@yahoo.fr>
2022-12-05 23:08:49 +0100

    suppression du code inutile pour l'ancien code barre de menu

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,7 @@
-import { useNavigate, Link } from "react-router-dom";
-import { useContext } from "react";
-
 import { useState, useEffect, useRef } from "react";
 
 import AuthContext from "../context/AuthProvider";
-import { faRightFromBracket, faUser, faHome } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { Routes, Route } from 'react-router-dom';
 import Header from '../components/Header';
 import CreatePost from '../components/Creation';
@@ -13,8 +9,6 @@ import PublishPost from '../components/PublishedPosts';
 
 
 const Home = () => {
-    /*const { setAuth } = useContext(AuthContext);
-    const navigate = useNavigate();*/
 
 
     /*Pour fermer le Dropdown menu*/
@@ -33,16 +27,6 @@ const Home = () => {
         }
     });
 
-    /* Pour se déconnecter 
-    const logout = async () => {
-        // if used in more components, this should be in context 
-        // axios to /logout endpoint 
-        setAuth({});
-        localStorage.clear();
-        navigate('/login');
-    }*/
-
-
 
 
 
@@ -55,13 +39,6 @@ const Home = () => {
             <section className="bg-light-grey section-bienvenue">
                 <h1>Bienvenue sur le Réseau Social de</h1>
                 <img className="logo-groupomania" src='/logo-rouge-centre-groupomania.png' alt='logo de Groupomania'></img>
-                {/*<br />
-                <div className="flexGrow">
-                    <button onClick={logout}>
-                        Se déconnecter
-                        <FontAwesomeIcon icon={faRightFromBracket} className="icone-a-droite"/>
-                    </button>
-    </div>*/}
             </section>
 
             <Routes>
